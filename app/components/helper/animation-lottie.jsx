4b44e108ce8a2cf453efd0e3d3402186c7a3c9ca
oscar <oscar.neveux@etudiant.univ-reims.fr>
2025-07-16 23:28:55 +0200

    Final SSR fixes - portfolio ready for Vercel deployment

--- app/components/helper/animation-lottie.jsx
+++ app/components/helper/animation-lottie.jsx
@@ -1,20 +1,45 @@
-"use client"
+"use client";
 
-import Lottie from "lottie-react";
+import { useEffect, useState } from "react";
 
 const AnimationLottie = ({ animationPath, width }) => {
+  const [Lottie, setLottie] = useState(null);
+
+  useEffect(() => {
+    // Import Lottie only on client side
+    import("lottie-react").then((module) => {
+      setLottie(() => module.default);
+    });
+  }, []);
+
+  if (!Lottie) {
+    return (
+      <div
+        style={{
+          width: "95%",
+          height: "200px",
+          backgroundColor: "#1a1443",
+          borderRadius: "8px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <div style={{ color: "#16f2b3", fontSize: "14px" }}>Loading...</div>
+      </div>
+    );
+  }
+
   const defaultOptions = {
     loop: true,
     autoplay: true,
     animationData: animationPath,
     style: {
-      width: '95%',
-    }
+      width: "95%",
+    },
   };
 
-  return (
-    <Lottie {...defaultOptions} />
-  );
+  return <Lottie {...defaultOptions} />;
 };
 
 export default AnimationLottie;
